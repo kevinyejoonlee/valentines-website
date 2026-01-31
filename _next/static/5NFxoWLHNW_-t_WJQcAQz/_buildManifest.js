fb94897ee8b2e73544f0a4a8d4f971015fd6ba08
kevinyejoonlee <kevinyejoonlee@users.noreply.github.com>
2026-01-31 21:52:14 +0000

    deploy: 3d3037197789d70bae38e0eedae7813ff152c738

---
+++ _next/static/5NFxoWLHNW_-t_WJQcAQz/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/valentines-website//_next/:path+",
+        "destination": "/valentines-website/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
